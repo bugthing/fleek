7c2ba7822d5addae7d6b8c5927ed8a4a92a9e5f1
Benjamin Martin <benjamin247365@hotmail.com>
2023-05-05 22:57:30 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -2,5 +2,17 @@
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
 
- 
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      # User specific aliases and functions
+      if [ -d ~/.bashrc.d ]; then
+        for rc in ~/.bashrc.d/*; do
+          if [ -f "$rc" ]; then
+            . "$rc"
+          fi
+        done
+      fi
+    '';
+  };
 }

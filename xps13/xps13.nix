dcce4983b77356259a2a79ab0e301772a6bce654
Benjamin Martin <benjamin247365@hotmail.com>
2023-05-15 09:07:06 +0100

    fleek: apply

---
+++ xps13/xps13.nix
@@ -0,0 +1,28 @@
+{ pkgs, misc, ... }: {
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+    home.username = "benjamin";
+    home.homeDirectory = "/home/benjamin";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "Benjamin Martin";
+        userEmail = "benjamin247365@hotmail.com";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "~/.ssh/bugthing";
+            signByDefault = builtins.stringLength "~/.ssh/bugthing" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+}

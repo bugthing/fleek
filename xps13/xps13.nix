f27422396935d398ee3ec98e8f4c016ecf0453ab
Benjamin Martin <benjamin247365@hotmail.com>
2023-05-15 09:11:55 +0100

    fleek: new system

--- xps13/xps13.nix
+++ xps13/xps13.nix
@@ -9,7 +9,7 @@
             graph = "log --decorate --oneline --graph";
             add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
         };
-        userName = "Benjamin Martin";
+        userName = "Ben Martin";
         userEmail = "benjamin247365@hotmail.com";
         extraConfig = {
             feature.manyFiles = true;

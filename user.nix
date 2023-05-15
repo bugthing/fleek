4528b2dd71bfd4b2f72ee73c97b602ef9865ff31
Benjamin Martin <benjamin247365@hotmail.com>
2023-05-15 09:46:54 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -2,4 +2,18 @@
   home.sessionVariables = {
     EDITOR = "nvim";
   };
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      # User specific aliases and functions
+      if [ -d ~/.bashrc.d ]; then
+        for rc in ~/.bashrc.d/*; do
+          if [ -f "$rc" ]; then
+            . "$rc"
+          fi
+        done
+      fi
+    '';
+  };
 }

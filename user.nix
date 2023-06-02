31ef02a31cfc30db3d974e9918e6d0088458c2f5
Benjamin Martin <benjamin247365@hotmail.com>
2023-06-02 18:24:09 +0100

    load custom bashrc stuff

--- user.nix
+++ user.nix
@@ -1,5 +1,18 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
- 
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      # User specific aliases and functions
+      if [ -d ~/.bashrc.d ]; then
+        for rc in ~/.bashrc.d/*; do
+          if [ -f "$rc" ]; then
+            . "$rc"
+          fi
+        done
+      fi
+    '';
+  };
+
 }

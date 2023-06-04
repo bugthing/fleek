8a7bd15fc9b978d73c165a9577be773bf78039a4
Benjamin Martin <benjamin247365@hotmail.com>
2023-06-04 19:04:48 +0100

    fleek: apply

--- README.md
+++ README.md
@@ -19,24 +19,4 @@ $ apply-$(hostname)
 ```
 
 Your actual aliases are listed below:
-    aptup = "sudo bash -c 'apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade'";
-
     fleeks = "cd ~/.local/share/fleek";
-
-    ga = "git add";
-
-    gb = "git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
-
-    gbr = "git branch";
-
-    gco = "git checkout";
-
-    gd = "git diff";
-
-    gs = "git status";
-
-    gvl = "git log | vim -c 'set keywordprg=git\ show' -R -";
-
-    lsblk = "lsblk -o name,fstype,size,label,uuid";
-
-    vcon = "nvim -p $(git status | grep 'both modified' | cut -d: -f2)";
--- aliases.nix
+++ aliases.nix
@@ -1,27 +1,7 @@
 { pkgs, misc, ... }: {
   # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
    home.shellAliases = {
-    aptup = "sudo bash -c 'apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade'";
-    
     fleeks = "cd ~/.local/share/fleek";
-    
-    ga = "git add";
-    
-    gb = "git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
-    
-    gbr = "git branch";
-    
-    gco = "git checkout";
-    
-    gd = "git diff";
-    
-    gs = "git status";
-    
-    gvl = "git log | vim -c 'set keywordprg=git\ show' -R -";
-    
-    lsblk = "lsblk -o name,fstype,size,label,uuid";
-    
-    vcon = "nvim -p $(git status | grep 'both modified' | cut -d: -f2)";
     
     # bat --plain for unformatted cat
     catp = "bat -P";
